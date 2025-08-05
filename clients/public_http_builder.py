9b995044ff04524e2a7e8315508fc11b5193cb65
Alexey Zubov <zubov.alexey@gmail.com>
2025-08-05 21:24:25 +0200

    Added Allure steps to capture and attach cURL commands to test reports

--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,5 +1,7 @@
 from httpx import Client
 
+from clients.event_hooks import curl_event_hook
+
 
 def get_public_http_client() -> Client:
     """
@@ -7,4 +9,8 @@ def get_public_http_client() -> Client:
 
     :return: Ready-to-use httpx.Client object.
     """
-    return Client(timeout=100, base_url="http://localhost:8000")
+    return Client(
+        timeout=100,
+        base_url="http://localhost:8000",
+        event_hooks={"request": [curl_event_hook]},
+    )
